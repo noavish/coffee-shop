292f1d8ffa967ef209fed971915d93a0c22d1680
noavish <noavish1@gmail.com>
2017-12-19 09:50:44 +0200

    INDIVIDUAL EXERCISE

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ var coffeeShop = {
 
   makeDrink: function (drinkType) {
     // TODO: Finish this method
+    var list = Object.keys(this.drinkRequirements);
+    for (var i=0; i<list.length; i++) {
+      if (list[i] === drinkType) {
+        if (this.beans >= this.drinkRequirements[list[i]]) {
+          this.beans -= this.drinkRequirements[list[i]];
+          console.log(this.beans);
+        } else {
+          console.log ("Sorry, we're all out of beans!");
+        }
+        return;
+      } 
+    } 
+    console.log ("Sorry, we don't make DRINKNAME");
   }
 }
 
